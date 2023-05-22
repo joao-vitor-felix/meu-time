668844a6ab4ceb5128ff59c1061feab16da16811
João Vitor <joaovitorfelixcontato@gmail.com>
2023-05-22 18:59:10 -0300

    perf: added code splitting strategy

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,51 @@
+import { lazy, Suspense } from "react";
 import { Theme } from "./styles/Theme";
 import { GlobalStyles } from "./styles/GlobalStyles";
 import { Route, Routes } from "react-router-dom";
-import Country from "./routes/Country/Country";
-import League from "./routes/League/League";
+import AuthContextProvider from "./contexts/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+import Spinner from "./components/Spinner/Spinner";
+
+const Country = lazy(() => import("./routes/Country/Country"));
+const League = lazy(() => import("./routes/League/League"));
+const Team = lazy(() => import("./routes/Team/Team"));
+const Auth = lazy(() => import("./routes/Auth/Auth"));
 
 function App() {
   return (
     <Theme>
       <GlobalStyles />
-      <Routes>
-        <Route path="/" element={<Country />} />
-        <Route path=":countryName" element={<League />} />
-      </Routes>
+      <AuthContextProvider>
+        <Suspense fallback={<Spinner />}>
+          <Routes>
+            <Route
+              index
+              element={
+                <ProtectedRoute>
+                  <Country />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path=":countryName"
+              element={
+                <ProtectedRoute>
+                  <League />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path=":countryName/:leagueId/:seasonId"
+              element={
+                <ProtectedRoute>
+                  <Team />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/auth" element={<Auth />} />
+          </Routes>
+        </Suspense>
+      </AuthContextProvider>
     </Theme>
   );
 }

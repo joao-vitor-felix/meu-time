f6849bcf888e2f9c33117e3392bcc395ccc916a3
João Vitor <joaovitorfelixcontato@gmail.com>
2023-05-28 20:41:04 -0300

    feat: added customized scrollbar

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -11,22 +11,43 @@ type DefaultTheme = {
 
 export const GlobalStyles = createGlobalStyle<{ theme: DefaultTheme }>`
 * {
-    margin: 0;
-    padding: 0;
-    box-sizing: inherit;
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
 }
 
 html {
-    font-size: 62.5%;
+  font-size: 62.5%;
+  scrollbar-width: thin;
+  scrollbar-color: ${({ theme }) => theme.colors.primary} ${({ theme }) =>
+  theme.colors.background};
+
+::-webkit-scrollbar {
+  width: 0.9vw;
 }
 
+::-webkit-scrollbar-thumb {
+  background-color: ${({ theme }) => theme.colors.primary};
+  border-radius: 10px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: #779e3f;
+}
+
+::-webkit-scrollbar-track {
+  background-color: ${({ theme }) => theme.colors.secondary};
+}
+  }
+
+
 body {
-    box-sizing: border-box;
-    font-family: 'Manrope', sans-serif;
-    background-color: ${({ theme }) => theme.colors.background};
-    height: 100vh;
-    width: 100vw;
-    color: ${({ theme }) => theme.colors.text};
+  box-sizing: border-box;
+  font-family: 'Manrope', sans-serif;
+  background-color: ${({ theme }) => theme.colors.background};
+  height: 100vh;
+  width: 100%;
+  color: ${({ theme }) => theme.colors.text};
 }
 
 `;

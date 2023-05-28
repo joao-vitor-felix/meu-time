a53281f17cc247082a3ac63144c28f2105d9b677
João Vitor <joaovitorfelixcontato@gmail.com>
2023-05-28 20:09:06 -0300

    feat: added ErrorBoundary component

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,39 @@
+import React, { ReactNode, ErrorInfo } from "react";
+
+interface ErrorBoundaryProps {
+  fallback: ReactNode;
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    console.log(error);
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: ErrorInfo) {
+    console.log(error, info.componentStack);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;

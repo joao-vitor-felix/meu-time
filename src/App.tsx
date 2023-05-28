e7e7c26966c2e971494cdd2f5bda6463f7a725c7
João Vitor <joaovitorfelixcontato@gmail.com>
2023-05-28 20:45:05 -0300

    feat: added ErrorBoundary strategy alongside final routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,47 +6,64 @@ import AuthContextProvider from "./contexts/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 import Spinner from "./components/Spinner/Spinner";
 
+const Auth = lazy(() => import("./routes/Auth/Auth"));
 const Country = lazy(() => import("./routes/Country/Country"));
 const League = lazy(() => import("./routes/League/League"));
 const Team = lazy(() => import("./routes/Team/Team"));
-const Auth = lazy(() => import("./routes/Auth/Auth"));
+const Details = lazy(() => import("./routes/Details/Details"));
+const ErrorBoundary = lazy(
+  () => import("./components/ErrorBoundary/ErrorBoundary")
+);
+const ErrorPage = lazy(() => import("./routes/ErrorPage/ErrorPage"));
+const NotFound = lazy(() => import("./routes/NotFound/NotFound"));
 
 function App() {
   return (
-    <Theme>
-      <GlobalStyles />
-      <AuthContextProvider>
-        <Suspense fallback={<Spinner />}>
-          <Routes>
-            <Route
-              index
-              element={
-                <ProtectedRoute>
-                  <Country />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path=":countryName"
-              element={
-                <ProtectedRoute>
-                  <League />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path=":countryName/:leagueId/:seasonId"
-              element={
-                <ProtectedRoute>
-                  <Team />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/auth" element={<Auth />} />
-          </Routes>
-        </Suspense>
-      </AuthContextProvider>
-    </Theme>
+    <ErrorBoundary fallback={<ErrorPage />}>
+      <Theme>
+        <GlobalStyles />
+        <AuthContextProvider>
+          <Suspense fallback={<Spinner />}>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <Country />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path=":countryName"
+                element={
+                  <ProtectedRoute>
+                    <League />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path=":countryName/:leagueId/:seasonId"
+                element={
+                  <ProtectedRoute>
+                    <Team />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path=":countryName/:leagueId/:seasonId/:teamId"
+                element={
+                  <ProtectedRoute>
+                    <Details />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/auth" element={<Auth />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
+        </AuthContextProvider>
+      </Theme>
+    </ErrorBoundary>
   );
 }
 
